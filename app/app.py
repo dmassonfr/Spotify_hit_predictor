1779a8af145f3f6ff7744fd19e46223474d50b1b
Paul Weber <paul.h.weber@gmail.com>
2023-03-23 15:08:51 +0100

    streamlit app new stlye

--- app/app.py
+++ app/app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import time
 import pandas as pd
 
 spotify_client_id = st.secrets['SPOTIPY_CLIENT_ID']
 spotify_client_secret = st.secrets['SPOTIPY_CLIENT_SECRET']
 
-st.sidebar.image('https://cdn-icons-png.flaticon.com/512/2424/2424869.png', width=60)
-st.sidebar.title('Spotify Music Search')
-
-#query = st.sidebar.text_input('Please enter your search query and hit Enter.', key="input")
+st.sidebar.image('https://cdn-icons-png.flaticon.com/512/408/408748.png', width=100)
+st.sidebar.title('Spotify Hit Predictor')
 
 @st.cache_data
 def get_select_box_data():
@@ -26,9 +23,6 @@ df = get_select_box_data()
 
 option = st.sidebar.selectbox('Select an artist', df['artist'])
 
-#filtered_df = df[df['artist'] == option]
-#st.write(filtered_df)
-
 lz_uri = df[df['artist'] == option]['lz_uri'].to_string(index = False)
 artist_name = df[df['artist'] == option]['artist'].to_string(index = False)
 
@@ -38,7 +32,7 @@ spotify = spotipy.Spotify(client_credentials_manager=
                                                    spotify_client_secret))
 results = spotify.artist_top_tracks(lz_uri)
 
-st.write('# TOP3 Tracks for ' + artist_name)
+st.markdown('# :green[TOP3 Tracks for ' + artist_name + "]")
 for track in results['tracks'][:3]:
   col1, col2 = st.columns([1, 2])
   with col1:
